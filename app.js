d243d04a8660bf7ffa7b4fd45477fd19546c1558
Jared Matson <40784129+Matsonj30@users.noreply.github.com>
2022-03-29 23:02:32 -0600

    help

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var startButton = document.getElementById(startBtn)
-startButton.addEventListener("click", )
+const startButton = document.getElementById('startBtn');
+startButton.addEventListener("click", initiateGame);
+
+function initiateGame(){
+    var gameButtons = document.getElementsByClassName('gameBtnScore');
+    for(i = 0; i < gameButtons.length; i+= 1){
+        for(j = 0; j < i; j+= 1);
+        console.log(gameButtons[i]);
+        //gameButtons[i].style.visibility = visible;
+    }//for
+}
