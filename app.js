01de8a20077c8c0d1b12c1aa4a54541efa73993b
Jared Matson <40784129+Matsonj30@users.noreply.github.com>
2022-03-31 14:58:29 -0600

    added comptuer choice

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 const startButton = document.getElementById('startBtn');
 startButton.addEventListener("click", initiateGame);
-console.log(startButton.innerHTML)
+
+
 function initiateGame(){
-    console.log('hi')
    startButton.style.visibility = 'hidden';
-    var gameButtons = document.getElementsByClassName('gameBtnandScore');
-    for(i = 0; i < gameButtons.length; i+= 1){
-        for(j = 0; j < i; j+= 1);
-        gameButtons[i].style.visibility = 'visible';
-    startGame()
+    var ButtonsAndScore = document.getElementsByClassName('gameBtnandScore');
+    var gameButtons = document.getElementsByClassName('gameButtons');
+    for(i = 0; i < ButtonsAndScore.length; i+= 1){ //make game buttons/scores visible
+        ButtonsAndScore[i].style.visibility = 'visible';
     }//for
-}
-function startGame(){
-    for(i = 0; i < 5; i += 1){ //5 rounds i guess?
-        console.log('ho')
-        console.log(Math.floor(Math.random()*3))
-    }
-}
+
+    for(j = 0; j < gameButtons.length; j += 1){            //if a choice is clicked, pass to funciton to see who won
+        gameButtons[j].addEventListener('click', function(){ //cant just put playerChoice up here as it will run when it compiles?
+            playerChoice(this.innerHTML); //try using "this"?
+        })
+    }//for
+
+}//initiateGame
+function playerChoice(playerChoice){
+    var playerScore = document.getElementById('playerScore')
+    var compScore = document.getElementById('computerScore')
+    var roundNum = document.getElementById('roundNum')
+    var compChoice = computerChoice()
+    console.log(playerChoice);
+    console.log(compChoice);
+   
+        
+}//startGame
+
+function computerChoice(){
+    switch(Math.floor(Math.random()*3)){
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    return null;
+    }//switch
+}//computerChoice
